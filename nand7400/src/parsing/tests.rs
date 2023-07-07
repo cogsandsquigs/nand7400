bef4aef833852a1e6e342512cb1005713e29281f
Ian Pratt <ianjdpratt@gmail.com>
2023-07-07 15:31:36 -0500

    TODO: more tests + AST

--- nand7400/src/parsing/tests.rs
+++ nand7400/src/parsing/tests.rs
@@ -3,9 +3,145 @@
 use super::{AssemblyParser, Rule};
 use pest::{consumes_to, parses_to};
 
+/// Test the parsing of a label.
+#[test]
+fn test_parse_label() {
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo:\n",
+        rule:   Rule::Label,
+        tokens: [
+            Label(0, 5, [
+                Identifier(0, 3),
+            ])
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo_bar_baz:",
+        rule:   Rule::Label,
+        tokens: [
+            Label(0, 12, [
+                Identifier(0, 11),
+                EOI(12, 12),
+            ])
+        ]
+    };
+}
+
+/// Test the parsing of an instruction.
+#[test]
+fn test_parse_instruction() {
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "nop\n",
+        rule:   Rule::Instruction,
+        tokens: [
+            Instruction(0, 4, [
+                Identifier(0, 3),
+            ])
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "JmP 0x1234",
+        rule:   Rule::Instruction,
+        tokens: [
+            Instruction(0, 10, [
+                Identifier(0, 3),
+                Literal(4, 10),
+                EOI(10, 10),
+            ])
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "ADD 0x1234 abc 456\n",
+        rule:   Rule::Instruction,
+        tokens: [
+            Instruction(0, 19, [
+                Identifier(0, 3),
+                Literal(4, 10),
+                Identifier(11, 14),
+                Literal(15, 18),
+            ])
+        ]
+    };
+}
+
+/// Test parsing an identifier.
+#[test]
+fn test_parse_identifier() {
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 3)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "fo0_b4r",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 7)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo_bar_baz",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 11)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo_bar_baz_",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 12)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo_bar_baz_123",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 15)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo_bar_baz_123_",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 16)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "foo_bar_baz_123_456",
+        rule:   Rule::Identifier,
+        tokens: [
+            Identifier(0, 19)
+        ]
+    };
+}
+
 /// Test parsing a literal value.
 #[test]
-fn test_literal() {
+fn test_parse_literal() {
     parses_to! {
         parser: AssemblyParser,
         input:  "012345689",
