380998f5ddd0c9466ef492bb848e9e7277cb4304
Ian Pratt <ianjdpratt@gmail.com>
2023-07-07 15:12:59 -0500

    aughe literals

--- nand7400/src/parsing/mod.rs
+++ nand7400/src/parsing/mod.rs
@@ -1,1 +1,9 @@
 mod tests;
+
+// use pest::Parser;
+use pest_derive::Parser;
+
+/// The main parsing structure to be used.
+#[derive(Parser)]
+#[grammar = "parsing/assembly.pest"]
+pub struct AssemblyParser {}
--- nand7400/src/parsing/tests.rs
+++ nand7400/src/parsing/tests.rs
@@ -1,1 +1,53 @@
 #![cfg(test)]
+
+use super::{AssemblyParser, Rule};
+use pest::{consumes_to, parses_to};
+
+/// Test parsing a literal value.
+#[test]
+fn test_literal() {
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "012345689",
+        rule:   Rule::Literal,
+        tokens: [
+            Literal(0, 9)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "0x0123456789ABCDEF",
+        rule:   Rule::Literal,
+        tokens: [
+            Literal(0, 18)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "0X0123456789abcdef",
+        rule:   Rule::Literal,
+        tokens: [
+            Literal(0, 18)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "0b101",
+        rule:   Rule::Literal,
+        tokens: [
+            Literal(0, 5)
+        ]
+    };
+
+    parses_to! {
+        parser: AssemblyParser,
+        input:  "0o01234567",
+        rule:   Rule::Literal,
+        tokens: [
+            Literal(0, 10)
+        ]
+    };
+}
