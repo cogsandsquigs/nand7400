ff178d18537ee2193b1a8757fad8943006b6ba22
Ian Pratt <ianjdpratt@gmail.com>
2023-07-17 08:30:02 -0500

    update docs + api

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 		),
 		.binaryTarget(
 			name: "Nand7400FFI",
-			url: "https://github.com/cogsandsquigs/nand7400/releases/download/v0.3.1/Nand7400FFI.xcframework.zip",
-			checksum: "3bcdfb390e55cd7f940a5fabb1944f8bb2fbf85791a3b8c55fbd1492d1c18428"
+			url: "https://github.com/cogsandsquigs/nand7400/releases/download/v0.3.2/Nand7400FFI.xcframework.zip",
+			checksum: "bcd5c6b14a72c8c94d33a63256a95bdd271c2636135beb1953d5cbc3e3054a06"
 		),
 		// .binaryTarget(
 		// 	name: "Nand7400FFI",
