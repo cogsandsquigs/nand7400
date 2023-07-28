6645e036d9ad6ae6f35671fd43af2ba22ae764f8
Ian Pratt <ianjdpratt@gmail.com>
2023-07-28 18:58:42 -0500

    comment [ci skip]

--- nand7400/src/parser/tests.rs
+++ nand7400/src/parser/tests.rs
@@ -1,1 +1,11 @@
 #![cfg(test)]
+
+use super::*;
+
+/// Test EOF parsing, and make sure that EOFs return the AST unchanged.
+fn parse_eof() {
+    let parser = Parser::new("").unwrap();
+    let ast = parser.parse().unwrap();
+
+    assert_eq!(ast, Ast::empty());
+}
