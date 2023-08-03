919d2002d9dd3ef65d3fc0aa7aa9ab428884badc
Ian Pratt <ianjdpratt@gmail.com>
2023-08-03 17:35:39 -0500

    alpha ver for testing of xcframework

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 		),
 		.binaryTarget(
 			name: "Nand7400FFI",
-			url: "https://github.com/cogsandsquigs/nand7400/releases/download/v0.4.0/Nand7400FFI.xcframework.zip",
+			url: "https://github.com/cogsandsquigs/nand7400/releases/download/v0.4.0-alpha.0/Nand7400FFI.xcframework.zip",
 			checksum: "bcd5c6b14a72c8c94d33a63256a95bdd271c2636135beb1953d5cbc3e3054a06"
 		),
 		// .binaryTarget(
