8a1cd86cff56cdf8a1519d3314e982c1c1ecd584
Ian Pratt <ianjdpratt@gmail.com>
2023-08-03 17:48:37 -0500

    upd swift pkg info

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
 		.binaryTarget(
 			name: "Nand7400FFI",
 			url: "https://github.com/cogsandsquigs/nand7400/releases/download/v0.4.0-alpha.0/Nand7400FFI.xcframework.zip",
-			checksum: "bcd5c6b14a72c8c94d33a63256a95bdd271c2636135beb1953d5cbc3e3054a06"
+			checksum: "cbeeba9a06057bb37add07e60a400761d28458cef0a4fa55185d6ac30ee979fe"
 		),
 		// .binaryTarget(
 		// 	name: "Nand7400FFI",
