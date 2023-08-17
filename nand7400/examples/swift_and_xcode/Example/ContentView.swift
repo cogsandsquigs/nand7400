514760fc752d64408150cf243bba6b8d6362c8de
Ian Pratt <ianjdpratt@gmail.com>
2023-08-17 17:34:59 -0500

    changes
    Signed-off-by: Ian Pratt <ianjdpratt@gmail.com>

--- nand7400/examples/swift_and_xcode/Example/ContentView.swift
+++ nand7400/examples/swift_and_xcode/Example/ContentView.swift
@@ -5,9 +5,9 @@
 //  Created by admin on 7/6/23.
 //
 
-import SwiftUI
 import Nand7400
 import class Nand7400.Formatter
+import SwiftUI
 
 let assemblyConf = AssemblerConfig(
 	opcodes: [
@@ -16,37 +16,37 @@ let assemblyConf = AssemblerConfig(
 		Opcode(mnemonic: "ldb", binary: 0x02, args: [OpcodeArg.indirect]),
 		Opcode(mnemonic: "add", binary: 0x03, args: [OpcodeArg.immediate, OpcodeArg.immediate, OpcodeArg.immediate]),
 		Opcode(mnemonic: "jmp", binary: 0x04, args: [OpcodeArg.immediate, OpcodeArg.immediate]),
-		Opcode(mnemonic: "hlt", binary: 0xFF, args: []),
+		Opcode(mnemonic: "hlt", binary: 0xff, args: []),
 	]
 )
 
 struct ContentView: View {
 	@State private var assemblyText = """
- ; Write some assembly...
- jmp LABEL
- nop
- nop
- 
- LABEL:
-  add #0x01 #0x02 #0x03
-  lda #-0x01
-  ldb +0x01
- """
+	; Write some assembly...
+	jmp LABEL
+	nop
+	nop
+
+	LABEL:
+	 add #0x01 #0x02 #0x03
+	 lda #-0x01
+	 ldb +0x01
+	"""
 	@State private var currentBinary = Data()
 	@State private var assembler = Assembler(config: assemblyConf)
 	@State private var formatter = Formatter()
 	@State private var errorMessage = ""
 	@State private var haveError = false
-	
+
 	var body: some View {
 		VStack {
 			VStack {
 				TextEditor(text: self.$assemblyText)
 					.font(Font.system(size: 15).monospaced())
 					.padding(.top, 5)
-				
+
 				Divider()
-				
+
 				Text(self.currentBinary.map { String(format: "0x%02X", $0) }.joined(separator: " "))
 					.font(Font.system(size: 15).monospaced())
 			}
@@ -54,7 +54,7 @@ struct ContentView: View {
 				RoundedRectangle(cornerRadius: 4)
 					.stroke(.blue, lineWidth: 2)
 			)
-			
+
 			HStack {
 				Button(action: {
 					do {
@@ -73,11 +73,11 @@ struct ContentView: View {
 				}
 				.alert(isPresented: self.$haveError) {
 					Alert(title: Text("An assembling error occured:"),
-						  message: Text(self.errorMessage),
-						  dismissButton: .default(Text("OK")))
+					      message: Text(self.errorMessage),
+					      dismissButton: .default(Text("OK")))
 				}
 				.padding()
-				
+
 				Button(action: {
 					self.assemblyText = self.formatter.format(source: self.assemblyText)
 				}) {
