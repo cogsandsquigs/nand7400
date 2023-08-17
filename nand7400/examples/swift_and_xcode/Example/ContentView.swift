7fd36a1dcdb809fdf35437e0477d3c3caaa022af
Ian Pratt <ianjdpratt@gmail.com>
2023-08-17 12:27:49 -0500

    update the examples

--- nand7400/examples/swift_and_xcode/Example/ContentView.swift
+++ nand7400/examples/swift_and_xcode/Example/ContentView.swift
@@ -13,28 +13,38 @@ let assemblyConf = AssemblerConfig(
 		Opcode(mnemonic: "nop", binary: 0x00, args: []),
 		Opcode(mnemonic: "lda", binary: 0x01, args: [OpcodeArg.immediate]),
 		Opcode(mnemonic: "ldb", binary: 0x02, args: [OpcodeArg.indirect]),
-		Opcode(mnemonic: "add", binary: 0x03, args: [OpcodeArg.immediate,OpcodeArg.immediate,OpcodeArg.immediate]),
+		Opcode(mnemonic: "add", binary: 0x03, args: [OpcodeArg.immediate, OpcodeArg.immediate, OpcodeArg.immediate]),
 		Opcode(mnemonic: "jmp", binary: 0x04, args: [OpcodeArg.immediate, OpcodeArg.immediate]),
-		Opcode(mnemonic: "hlt", binary: 0xff, args: []),
+		Opcode(mnemonic: "hlt", binary: 0xFF, args: []),
 	]
 )
 
 struct ContentView: View {
-	@State private var assemblyText = "; Write some assembly...\njmp LABEL\nnop\nnop\n\nLABEL:\n\tadd #0x01 #0x02 #0x03\n\tlda #-0x01\n\tldb +0x01"
-	@State private var currentBinary: Data = .init()
+	@State private var assemblyText = """
+	; Write some assembly...
+	jmp LABEL
+	nop
+	nop
+
+	LABEL:
+		add #0x01 #0x02 #0x03
+		lda #-0x01
+		ldb +0x01
+	"""
+	@State private var currentBinary: Data = Data()
 	@State private var assembler = Assembler(config: assemblyConf)
 	@State private var errorMessage: String = ""
 	@State private var haveError = false
-	
+
 	var body: some View {
 		VStack {
 			VStack {
 				TextEditor(text: self.$assemblyText)
 					.font(Font.system(size: 15).monospaced())
 					.padding(.top, 5)
-				
+
 				Divider()
-				
+
 				Text(self.currentBinary.map { String(format: "0x%02X", $0) }.joined(separator: " "))
 					.font(Font.system(size: 15).monospaced())
 			}
@@ -46,8 +56,7 @@ struct ContentView: View {
 				do {
 					self.currentBinary = try assembler.assemble(source: assemblyText)
 					print(self.currentBinary.map { String(format: "0x%02X", $0) }.joined(separator: " "))
-				}
-				catch {
+				} catch {
 					self.errorMessage = "An error occured!"
 					self.haveError = true
 					print(error)
@@ -60,8 +69,8 @@ struct ContentView: View {
 			}
 			.alert(isPresented: self.$haveError) {
 				Alert(title: Text("An assembling error occured:"),
-					  message: Text(self.errorMessage),
-					  dismissButton: .default(Text("OK")))
+				      message: Text(self.errorMessage),
+				      dismissButton: .default(Text("OK")))
 			}
 			.padding()
 		}
